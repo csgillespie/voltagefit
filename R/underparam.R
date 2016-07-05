89dadcc0c89b7c1ca2878b60619c4aea845867a2
gawhitaker <g.a.whitaker@ncl.ac.uk>
2016-07-05 10:50:31 +0100

    Changes to param.R to reduce allocating, weekparam and underparam functions added

---
+++ R/underparam.R
@@ -0,0 +1,60 @@
+## function to create underlying parameters
+## fited is of the form, fitwafer
+
+underparam = function(wafer,fitted)
+{
+  theta = thetasetup(wafer,fitted)
+  
+  options(contrasts=c("contr.sum","contr.sum"))
+  man = manova(cbind(X1,X2,X3,X4,X5,X6) ~ factor(w)+factor(n), data=theta$forward,weights=1/fitted$forward$cost) 
+  
+  t_all=c(man$coefficients[1,1],
+          man$coefficients[1,2],
+          man$coefficients[1,3],
+          man$coefficients[1,4],
+          man$coefficients[1,5],
+          man$coefficients[1,6])
+  
+  m=lm(cbind(X1,X2,X3,X4,X5,X6) ~ factor(w)+factor(n), data=theta$forward)
+  vmat=vcov(m)
+  
+  loc=NULL
+  for(i in 1:6)
+  {
+    loc = c(loc,1+(i-1)*(length(unique(wafer$test_date))+length(unique(wafer$name)))-(i-1)) 
+  }
+  
+  t_vmat=matrix(0,ncol=6,nrow=6)
+  for(i in 1:6)
+  {
+    t_vmat[i,]=vmat[loc[i],loc]
+  }  
+  forward = list(param = t_all,var = t_vmat)
+  
+  man = manova(cbind(X1,X2,X3,X4,X5,X6) ~ factor(w)+factor(n), data=theta$backward,weights=1/fitted$forward$cost) 
+  
+  t_all=c(man$coefficients[1,1],
+          man$coefficients[1,2],
+          man$coefficients[1,3],
+          man$coefficients[1,4],
+          man$coefficients[1,5],
+          man$coefficients[1,6])
+  
+  m=lm(cbind(X1,X2,X3,X4,X5,X6) ~ factor(w)+factor(n), data=theta$backward)
+  vmat=vcov(m)
+  
+  loc=NULL
+  for(i in 1:6)
+  {
+    loc = c(loc,1+(i-1)*(length(unique(wafer$test_date))+length(unique(wafer$name)))-(i-1)) 
+  }
+  
+  t_vmat=matrix(0,ncol=6,nrow=6)
+  for(i in 1:6)
+  {
+    t_vmat[i,]=vmat[loc[i],loc]
+  }  
+  backward = list(param = t_all,var = t_vmat)
+  
+  return(list(forward=forward,backward=backward))
+}
