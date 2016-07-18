29d36aee506eb6478e3e2c308c7aa6eb6716ae14
gawhitaker <g.a.whitaker@ncl.ac.uk>
2016-07-18 14:01:09 +0100

    added function to perform MANOVA

---
+++ R/fitmanova.R
@@ -0,0 +1,23 @@
+## function to fit manova to the results of fitall
+##
+## fitall is of the form fitall, design is the design matrix with columns 
+## week, wafer, replicate, treatment (ordered as such)
+
+fitmanova = function(fitall,design)
+{
+  options(contrasts=c("contr.sum","contr.sum"))
+  
+  fdata = cbind(week=design[,1],fitall$forward$parameters)
+  bdata = cbind(week=design[,1],fitall$backward$parameters)
+  
+  man_w_f = manova(cbind(X1,X2,X3,X4,X5,X6) ~ factor(week)+factor(name), data=fdata,weights=1/fitall$forward$cost$cost) 
+  m_f = lm(cbind(X1,X2,X3,X4,X5,X6) ~ factor(week)+factor(name), data=fdata)
+
+  man_w_b = manova(cbind(X1,X2,X3,X4,X5,X6) ~ factor(week)+factor(name), data=bdata,weights=1/fitall$forward$cost$cost) 
+  m_b = lm(cbind(X1,X2,X3,X4,X5,X6) ~ factor(week)+factor(name), data=bdata)
+  
+  weight = list(forward = man_w_f,backward = man_w_b)
+  noweight = list(forward = m_f,backward = m_b)
+  
+  return(list(weight=weight,noweight=noweight))
+}
