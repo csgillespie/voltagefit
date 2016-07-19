6df2e4c466558eb72ef4069c2559ac5f645bd259
gawhitaker <g.a.whitaker@ncl.ac.uk>
2016-07-19 11:17:47 +0100

    lim changed to iterlim

--- R/model.R
+++ R/model.R
@@ -1,10 +1,10 @@
 ## function to fit logcurve to the wafer, getting parameters and cost
-## wafer is the data to fit to (a single wafer), lim is the max number 
+## wafer is the data to fit to (a single wafer), iterlim is the max number 
 ## of iterations to use in nlm (default=2000), id is the wafer id (file name)
 ##
 ## the function returns a list with elements parameters and cost
 
-fitwafer = function(wafer,lim=2000,id=NULL)
+fitwafer = function(wafer,iterlim=2000,id=NULL)
 {
   if(is.null(id)){id = randomStrings(n=1, len=5)} #this feels slow, poss other function?
   else{id=gsub(".rds","",id)}
@@ -30,19 +30,19 @@ fitwafer = function(wafer,lim=2000,id=NULL)
     
     #forwards
     m.nlmf = nlm(minlogcurve,c(1.0,1.4,-3,4,-1.5,0.3),d_forward$VG,
-                 max(log(abs(d_forward$ID)))/log(abs(d_forward$ID)),iterlim=lim) 
+                 max(log(abs(d_forward$ID)))/log(abs(d_forward$ID)),iterlim=iterlim) 
     
     estf[place,] = m.nlmf$estimate
-    if(m.nlmf$iterations==lim) message(paste("Max reached for forward, device ",j,".",k,".",i))
+    if(m.nlmf$iterations==iterlim) message(paste("Max reached for forward, device ",j,".",k,".",i))
     minf[place,] = minlogcurve(m.nlmf$estimate,d_forward$VG,
                               max(log(abs(d_forward$ID)))/log(abs(d_forward$ID)))
     
     #backwards
     m.nlmb = nlm(minlogcurveb,c(0.01,0.01,0.01,0.01,0.01,0.01),d_backward$VG,
-                 max(log(abs(d_backward$ID)))/log(abs(d_backward$ID)),m.nlmf$estimate,iterlim=lim)
+                 max(log(abs(d_backward$ID)))/log(abs(d_backward$ID)),m.nlmf$estimate,iterlim=iterlim)
     
     estb[place,] = m.nlmb$estimate
-    if(m.nlmb$iterations==lim) message(paste("Max reached for backward, device ",j,".",k,".",i))
+    if(m.nlmb$iterations==iterlim) message(paste("Max reached for backward, device ",j,".",k,".",i))
     minb[place,] = minlogcurveb(m.nlmb$estimate,d_backward$VG,
                                max(log(abs(d_backward$ID)))/log(abs(d_backward$ID)),m.nlmf$estimate)
   }
@@ -59,7 +59,7 @@ fitwafer = function(wafer,lim=2000,id=NULL)
 ## the function returns a list with elements parameters and cost
 ## each wafer should be suitably filtered and formated before been fed in
 
-fitall = function(path,lim=2000)
+fitall = function(path,iterlim=2000)
 {
   files = list.files(path = path, pattern = ".rds")
   message("files read: ",paste(files,collapse=" "))
@@ -68,7 +68,7 @@ fitall = function(path,lim=2000)
   {
     wafer = readRDS(paste(path,files[i],sep="")) 
     message("wafer: ",files[i])
-    temp = fitwafer(wafer,lim,files[i])
+    temp = fitwafer(wafer,iterlim,files[i])
     
     if(i==1)
     {
